b75b16ff5c37ba6c8f8008a45ee91283eda0a260
predmaxim <predmaxim@ya.ru>
2022-10-01 22:30:02 +0500

    fix: donation form slider

---
+++ online-zoo/src/blocks/forms/donation-form/donation-form.js
@@ -0,0 +1,24 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const range = document.querySelector('#range__range-input');
+  const amount = document.querySelector('#amount');
+  const rangeValues = document.querySelectorAll('#range-values option');
+
+  const values = [25, 50, 100, 250, 500, 1000, 2000, 5000]
+
+  document.addEventListener('input', (e) => {
+    e.target == range ? amount.value = values[range.value - 1] : false;
+    e.target == amount ? range.value = values.reduce((acc, el, i) => el == amount.value ? i + 1 : acc, 0) : false;
+  });
+
+  document.addEventListener('click', (e) => {
+    rangeValues.forEach(elem => {
+      if (e.target == elem) {
+
+        amount.value = values[e.target.value - 1];
+        range.value = e.target.value;
+      };
+    });
+  });
+})
+
+export default {}
---
+++ online-zoo/src/blocks/forms/forms.js
@@ -0,0 +1,1 @@
+import { } from "./donation-form/donation-form";
