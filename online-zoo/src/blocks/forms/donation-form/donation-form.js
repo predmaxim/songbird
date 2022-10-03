e434d3d50c3004bdfbdd24d9b800ae942bc99a59
predmaxim <predmaxim@ya.ru>
2022-10-03 09:24:21 +0500

    feat: add active state to range labels

--- online-zoo/src/blocks/forms/donation-form/donation-form.js
+++ online-zoo/src/blocks/forms/donation-form/donation-form.js
@@ -1,27 +1,47 @@
 window.addEventListener("DOMContentLoaded", () => {
   const range = document.querySelector('#range__range-input');
   const amount = document.querySelector('#amount');
-  const rangeValues = document.querySelectorAll('#range-values option');
+  const rangeItems = document.querySelectorAll('#range-values option');
 
-  const values = [25, 50, 100, 250, 500, 1000, 2000, 5000]
-
+  const labelValues = [...rangeItems].reduce((acc, el) => {
+    return acc + (el.label).slice(1) + ',';
+  }, '').split(',').filter((el) => el !== '').sort((a, b) => a - b);
 
   document.addEventListener('input', (e) => {
+
     if (e.target.value.length > 4) {
       e.target.value = e.target.value.slice(0, 4);
     }
-    e.target == range ? amount.value = values[range.value - 1] : false;
-    e.target == amount ? range.value = values.reduce((acc, el, i) => el == amount.value ? i + 1 : acc, 3) : false;
+
+    if (e.target == range) {
+
+      amount.value = labelValues[range.value - 1];
+
+      if (labelValues.some((el) => el == labelValues[range.value - 1])) {
+        [...rangeItems].forEach(el => el.classList.remove('active'));
+        [...rangeItems].filter(el => el.label.slice(1) == labelValues[e.target.value - 1])[0].classList.add('active');
+      } else { [...rangeItems].forEach(el => el.classList.remove('active')); }
+
+    }
+
+    if (e.target == amount) {
+      range.value = labelValues.reduce((acc, el, i) => el == amount.value ? i + 1 : acc, 3);
+
+      if (labelValues.some(el => el == amount.value)) {
+        [...rangeItems].forEach(el => el.classList.remove('active'));
+        [...rangeItems].filter(el => el.label.slice(1) == amount.value)[0].classList.add('active');
+      } else { [...rangeItems].forEach(el => el.classList.remove('active')); }
+    }
+
   });
 
   document.addEventListener('click', (e) => {
-    rangeValues.forEach(elem => {
-      if (e.target == elem) {
-
-        amount.value = values[e.target.value - 1];
-        range.value = e.target.value;
-      };
-    });
+    if ([...rangeItems].some(el => el == e.target)) {
+      [...rangeItems].forEach(el => el.classList.remove('active'));
+      [...rangeItems].filter(el => el == e.target)[0].classList.add('active');
+      amount.value = labelValues[e.target.value - 1];
+      range.value = e.target.value;
+    }
   });
 })
 
