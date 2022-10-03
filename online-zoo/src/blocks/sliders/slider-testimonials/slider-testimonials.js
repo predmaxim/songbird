2d4a2a300ea1b2ad5b9fc7c1fd041e0ff4476907
predmaxim <predmaxim@ya.ru>
2022-10-04 03:13:06 +0500

    feat: add testimonials slider scrolling

--- online-zoo/src/blocks/sliders/slider-testimonials/slider-testimonials.js
+++ online-zoo/src/blocks/sliders/slider-testimonials/slider-testimonials.js
@@ -2,13 +2,16 @@ import data from "./data";
 
 document.addEventListener('DOMContentLoaded', () => {
 
-  const container = document.querySelector('.testimonials-container');
-  // const items = container.querySelectorAll('.item');
-  // const comment = container.querySelector('.item__text');
-  // const userName = container.querySelector('.name');
-  // const location = container.querySelector('.location');
-  // const date = container.querySelector('.date');
-  // const avatar = container.querySelector('.avatar').src;
+  const container = document.querySelector('.testimonials__slider');
+  const slider = document.querySelector('.testimonials-container');
+  let thumbBack = document.querySelector('.slider-pagination');
+  let thumb = document.querySelector('.thumb');
+  let sliderItems = document.querySelectorAll('.testimonials-container .item');
+  let visibleItemsNumber = 0;
+
+  let percentage = 0;
+  let itemWidth = 0;
+
   const itemElem = `
     <div class="item">
       <div class="item__header" >
@@ -25,20 +28,72 @@ document.addEventListener('DOMContentLoaded', () => {
       <div class="item__wrapper"></div>
     </div>`
 
-  const setSlider = () => {
-    for (let i = 0; i < data.length; i++) {
-      container.insertAdjacentHTML('afterbegin', itemElem);
-      container.querySelector('.avatar').src = data[i].avatar;
-      container.querySelector('.name').textContent = data[i].name;
-      container.querySelector('.location').textContent = data[i].location;
-      container.querySelector('.date').textContent = data[i].date;
-      container.querySelector('.item__text').textContent = data[i].comment;
-      container.querySelector('.item').classList.add(`user-${data[i].id}`);
-    }
-
+  for (let i = 0; i < data.length; i++) {
+    slider.insertAdjacentHTML('afterbegin', itemElem);
+    slider.querySelector('.avatar').src = data[i].avatar;
+    slider.querySelector('.name').textContent = data[i].name;
+    slider.querySelector('.location').textContent = data[i].location;
+    slider.querySelector('.date').textContent = data[i].date;
+    slider.querySelector('.item__text').textContent = data[i].comment;
+    slider.querySelector('.item').classList.add(`user-${data[i].id}`);
   }
 
-  setSlider()
+  function init() {
+    sliderItems = document.querySelectorAll('.testimonials-container .item');
+    itemWidth = sliderItems[0].offsetWidth;
+
+    visibleItemsNumber = Math.round(container.offsetWidth / itemWidth);
+
+    thumbBack = document.querySelector('.slider-pagination');
+    thumb = document.querySelector('.thumb');
+
+    percentage = (visibleItemsNumber / sliderItems.length * 100);
+
+    thumb.style.width = percentage + '%';
+  }
+
+  init()
+
+
+  thumb.onmousedown = function (event) {
+    event.preventDefault(); // предотвратить запуск выделения (действие браузера)
+
+    let shiftX = event.clientX - thumb.getBoundingClientRect().left;
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+
+    function onMouseMove(event) {
+      let newLeft = event.clientX - shiftX - thumbBack.getBoundingClientRect().left;
+
+      if (newLeft < 0) {
+        newLeft = 0;
+      }
+
+      let rightEdge = thumbBack.offsetWidth - thumb.offsetWidth;
+
+      if (newLeft > rightEdge) {
+        newLeft = rightEdge;
+      }
+
+      thumb.style.left = newLeft + 'px';
+      slider.style.transition = `1s`;
+      slider.style.transform = `translateX(${-newLeft * 3.81}px)`;
+    }
+
+    function onMouseUp() {
+      document.removeEventListener('mouseup', onMouseUp);
+      document.removeEventListener('mousemove', onMouseMove);
+    }
+
+  };
+
+  thumb.ondragstart = function () {
+    return false;
+  };
+
+
+  window.addEventListener('resize', init)
 
 })
 
