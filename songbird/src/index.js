d547832cc168e81a81de23338a07c223328b07a0
Maxim Predybailo <predmaxim@ya.ru>
2022-11-17 09:07:33 +0500

    feat: add imports

--- songbird/src/index.js
+++ songbird/src/index.js
@@ -1,7 +1,7 @@
 import './components/birds-description/birds-description.js';
 import './components/birds-list/birds-list.js';
-import './components/page-result/page-result.js';
+import './components/result/result.js';
 import './components/player/player.js';
-import './components/score-block/score-block.js';
-import './components/settings-menu/settings-menu.js';
-
+import './components/score/score.js';
+import './components/settings/settings.js';
+console.log('Hello!')
