71e9b268caf4ff08eb2f4ed50476c6dcbd91aa44
Maxim Predybailo <predmaxim@ya.ru>
2022-11-21 23:59:50 +0500

    feat: add slider functions

---
+++ songbird/src/components/slider/slider.js
@@ -0,0 +1,36 @@
+import { newGame } from 'Base/base.js';
+
+const homePlayBtn = document.querySelector('button.songbird-home-play-btn');
+const newGameBtn = document.querySelector('button.new-game-btn');
+const slider = document.querySelector('.slider');
+let sliderWidth = document.body.offsetWidth;
+let curSlider = 0;
+
+const sliderRoll = (toSlide) => {
+
+  if(toSlide) curSlider = toSlide;
+  else curSlider++;
+
+  slider.style.transform = `translateX(-${curSlider * sliderWidth}px)`;
+}
+
+
+document.addEventListener('click', (e) => {
+  if (e.target === homePlayBtn) {
+    sliderRoll();
+    newGame();
+  };
+
+  if (e.target === newGameBtn) {
+    sliderRoll(1);
+    newGame();
+  }
+
+})
+
+window.addEventListener('resize', () => {
+  sliderWidth = document.body.offsetWidth;
+  slider.style.transform = `translateX(-${curSlider * sliderWidth}px)`;
+});
+
+export { sliderRoll };
